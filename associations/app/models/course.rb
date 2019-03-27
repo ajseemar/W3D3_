c0fd7c337554285cfb3009040964bbd85a409cd3
ajseemar <ajseemar@gmail.com>
2019-03-27 15:22:30 -0700

    uncomment

--- associations/app/models/course.rb
+++ associations/app/models/course.rb
@@ -17,19 +17,19 @@ class Course < ApplicationRecord
     foreign_key: :course_id,
     class_name: :Enrollment   
     
-    # has_many :enrolled_students,
-    # through: :enrollments,
-    # source: :user
+    has_many :enrolled_students,
+    through: :enrollments,
+    source: :user
 
-    # belongs_to :prerequisite,
-    # primary_key: :id,
-    # foreign_key: :prereq_id,
-    # class_name: :Course 
+    belongs_to :prerequisite,
+    primary_key: :id,
+    foreign_key: :prereq_id,
+    class_name: :Course 
 
-    # belongs_to :instructor,
-    # primary_key: :id,
-    # foreign_key: :instructor_id,
-    # class_name: :User
+    belongs_to :instructor,
+    primary_key: :id,
+    foreign_key: :instructor_id,
+    class_name: :User
 
     
 end
